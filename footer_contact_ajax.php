ca27062b477265ad982f0263a01d880bb1b9d2e4
Evergreen Web Design <robert@robertmeans.com>
2021-10-23 18:41:58 -0600

    prosperity commit

--- footer_contact_ajax.php
+++ footer_contact_ajax.php
@@ -10,49 +10,61 @@
   $name = trim($_POST['name']);
   $email = trim($_POST['email']);
   $message = trim($_POST['comments']);
+  $words = str_word_count($message);
 
 if (is_post_request()) {
 
   if($name && $email && $message) {
 
-    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if ($words > 2) {
 
 
-    $mail = new PHPMailer(true);
+      if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
 
-    try { 
+      $mail = new PHPMailer(true);
 
-        $mail->Host = 'localhost';
-        $mail->Username   = EMAIL;
-        $mail->Password   = PASSWORD; 
-        $mail->Port = 587;
-        // email routing set to automatically detect
+      try { 
 
-        //Recipients
-        $mail->setFrom(EMAIL, 'MotherfuckingBob.com');
-        $mail->addAddress(EMAIL, 'MotherfuckingBob.com Website');     // Add a recipient
-        $mail->addReplyTo($email, $name);
-        // $mail->addCC('cc@example.com');
-        // $mail->addBCC('robertmeans01@gmail.com');
+          $mail->Host = 'localhost';
+          $mail->Username   = EMAIL;
+          $mail->Password   = PASSWORD; 
+          $mail->Port = 587;
+          // email routing set to automatically detect
 
-        // Content
-        $mail->isHTML(true);
-        $mail->Subject = 'Email from MotherfuckingBob.com Website';
-        $mail->Body    =  'Name: ' . $name . '<br>Email: ' . $email . '<br><hr><br><br>' . nl2br($message);
+          //Recipients
+          $mail->setFrom(EMAIL, 'MotherfuckingBob.com');
+          $mail->addAddress(EMAIL, 'MotherfuckingBob.com Website');     // Add a recipient
+          $mail->addReplyTo($email, $name);
+          // $mail->addCC('cc@example.com');
+          // $mail->addBCC('robertmeans01@gmail.com');
 
-        $mail->send();
-        // echo 'Message has been sent';
-        $signal = 'ok';
-        $msg =  'Message sent successfully';
-      } catch (Exception $e) {
-          $signal = 'bad';
-          $msg = 'Mail Error: {$mail->ErrorInfo}';
+          // Content
+          $mail->isHTML(true);
+          $mail->Subject = 'Email from MotherfuckingBob.com Website';
+          $mail->Body    =  'Name: ' . $name . '<br>Email: ' . $email . '<br><hr><br><br>' . nl2br($message);
+
+          $mail->send();
+          // echo 'Message has been sent';
+          $signal = 'ok';
+          $msg =  'Message sent successfully';
+        } catch (Exception $e) {
+            $signal = 'bad';
+            $msg = 'Mail Error: {$mail->ErrorInfo}';
+        }
+
+      } else {
+        $signal = 'bad';
+        $msg = 'Invalid email address. Please fix.';
       }
+
+
 
     } else {
       $signal = 'bad';
-      $msg = 'Invalid email address. Please fix.';
+      $msg = 'Are you a real person? The 1 and 2 word messages typically suggest a bot aiming to inject malware. Type a little something just so I can make this slightly more complicted for those with nefarious motives. :)';
     }
+
+
 
   } else {
     $signal = 'bad';
